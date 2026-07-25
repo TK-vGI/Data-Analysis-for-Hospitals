e8516c664eddc309db5445957baafb4ea46968b0
Tomáš Kaňuch <tomas.kanuch@gmail.com>
2026-07-25 16:09:43 +0200

    Stage 2

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -9,6 +9,19 @@ dfGeneral = pd.read_csv('test/general.csv')
 dfPrenatal = pd.read_csv('test/prenatal.csv')
 dfSports = pd.read_csv('test/sports.csv')
 
-print(dfGeneral.head(20))
-print(dfPrenatal.head(20))
-print(dfSports.head(20))
+# print(dfGeneral.head(20))
+# print(dfPrenatal.head(20))
+# print(dfSports.head(20))
+
+"""
+2/5: Merging dataframes, modifying columns`names, drop column
+"""
+columns = list(dfGeneral.columns.values) # creating 'columns' names list from General
+dfPrenatal.columns = columns
+dfSports.columns = columns
+
+dfMerged = pd.concat([dfGeneral,dfPrenatal,dfSports],ignore_index=True)
+# print(dfMerged.columns.values)
+dfMerged.drop('Unnamed: 0', axis=1, inplace=True)
+# print(dfMerged.head(20))
+print(dfMerged.sample(n=20, random_state=30))
